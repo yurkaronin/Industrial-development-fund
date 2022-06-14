fbb47b554797038c46fdb1f9c3c411f114f6a2bf
RoninsWork <yurkaronin@gmail.com>
2022-06-14 18:20:33 +0300

    Добавил новые секции на главной

--- js/parts/sliders.js
+++ js/parts/sliders.js
@@ -1,9 +1,4 @@
 const swiper = new Swiper(".mySwiper", {
-  // slidesPerView: 6,
-  // spaceBetween: 32,
-  // loopedSlides: 6,
-  // slidesPerView: "auto",
-  // centeredSlides: true,
   navigation: {
     nextEl: ".js-slider-next",
     prevEl: ".js-slider-prew",
@@ -29,6 +24,77 @@ const swiper = new Swiper(".mySwiper", {
     1440: {
       slidesPerView: 6,
       spaceBetween: 32
+    }
+  },
+  loop: true,
+  // autoplay: true,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: true,
+});
+
+
+const swiper2 = new Swiper(".mySwiper2", {
+  /* navigation: {
+    nextEl: ".mySwiper2 .js-slider-next",
+    prevEl: ".mySwiper2 .js-slider-prew",
+  }, */
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 425px
+    425: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 0
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 0
+    },
+    // when window width is >= 1440px
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 0
+    }
+  },
+  loop: true,
+  // autoplay: true,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: true,
+});
+
+const swiper3 = new Swiper(".mySwiper3", {
+ navigation: {
+    nextEl: ".js-slider-stories .js-slider-next",
+    prevEl: ".js-slider-stories .js-slider-prew",
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 425px
+    425: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 0
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 0
+    },
+    // when window width is >= 1440px
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 0
     }
   },
   loop: true,
