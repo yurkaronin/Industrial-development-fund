e9f7649c907146835e25d552a5aa1511e559cdd3
RoninsWork <yurkaronin@gmail.com>
2022-06-15 11:55:44 +0300

    Доработал слайдер ПАРТНЁРЫ

--- js/parts/sliders.js
+++ js/parts/sliders.js
@@ -3,33 +3,11 @@ const swiper = new Swiper(".mySwiper", {
     nextEl: ".js-slider-next",
     prevEl: ".js-slider-prew",
   },
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 425px
-    425: {
-      slidesPerView: 1,
-      spaceBetween: 16
-    },
-    // when window width is >= 768px
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 16
-    },
-    // when window width is >= 1024px
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 32
-    },
-    // when window width is >= 1440px
-    1440: {
-      slidesPerView: 6,
-      spaceBetween: 32
-    }
-  },
   loop: true,
   // autoplay: true,
   grabCursor: true,
   slidesPerView: "auto",
+  spaceBetween: 32,
   centeredSlides: true,
 });
 
@@ -40,29 +18,6 @@ const swiper2 = new Swiper(".mySwiper2", {
     prevEl: ".mySwiper2 .js-slider-prew",
   }, */
   centeredSlides: true,
-  // Responsive breakpoints
-  // breakpoints: {
-    // when window width is >= 425px
-    // 425: {
-    //   slidesPerView: 1,
-    //   spaceBetween: 0
-    // },
-    // when window width is >= 768px
-    // 768: {
-    //   slidesPerView: 1,
-    //   spaceBetween: 0
-    // },
-    // when window width is >= 1024px
-    // 1024: {
-    //   slidesPerView: 2,
-    //   spaceBetween: 0
-    // },
-    // when window width is >= 1440px
-    // 1440: {
-    //   slidesPerView: 3,
-    //   spaceBetween: 0
-    // }
-  // },
   loop: true,
   // autoplay: true,
   grabCursor: true,
@@ -75,29 +30,6 @@ const swiper3 = new Swiper(".mySwiper3", {
     nextEl: ".js-slider-stories .js-slider-next",
     prevEl: ".js-slider-stories .js-slider-prew",
   },
-  // Responsive breakpoints
-  // breakpoints: {
-    // when window width is >= 425px
-    // 425: {
-    //   slidesPerView: 1,
-    //   spaceBetween: 32
-    // },
-    // when window width is >= 768px
-    // 768: {
-    //   slidesPerView: 2,
-    //   spaceBetween: 32
-    // },
-    // when window width is >= 1024px
-    // 1024: {
-    //   slidesPerView: 3,
-    //   spaceBetween: 32
-    // },
-    // when window width is >= 1440px
-  //   1440: {
-  //     slidesPerView: 4,
-  //     spaceBetween: 32
-  //   }
-  // },
   loop: true,
   // autoplay: true,
   grabCursor: true,
