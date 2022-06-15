0526d3b45b123f72a8620babbf2b7a06d03892b3
RoninsWork <yurkaronin@gmail.com>
2022-06-15 16:07:24 +0300

    Закончил десктоп

--- js/parts/sliders.js
+++ js/parts/sliders.js
@@ -13,10 +13,14 @@ const swiper = new Swiper(".mySwiper", {
 
 
 const swiper2 = new Swiper(".mySwiper2", {
-  /* navigation: {
+  navigation: {
     nextEl: ".mySwiper2 .js-slider-next",
-    prevEl: ".mySwiper2 .js-slider-prew",
-  }, */
+    prevEl: ".mySwiper2 .js-slider-prev",
+  },
+  pagination: {
+    el: ".mySwiper2 .swiper-pagination",
+    clickable: true,
+  },
   centeredSlides: true,
   loop: true,
   // autoplay: true,
@@ -43,6 +47,22 @@ const swiper4 = new Swiper(".mySwiper4", {
      nextEl: ".js-slider-news .js-slider-next",
      prevEl: ".js-slider-news .js-slider-prew",
    },
+   loop: true,
+   // autoplay: true,
+   grabCursor: true,
+   slidesPerView: "auto",
+   spaceBetween: 32,
+   centeredSlides: true,
+ });
+
+ const swiper5 = new Swiper(".mySwiper5", {
+  navigation: {
+     nextEl: ".js-slider-promo .js-slider-next",
+   },
+   pagination: {
+    el: ".js-slider-promo .swiper-pagination",
+    type: "fraction",
+  },
    loop: true,
    // autoplay: true,
    grabCursor: true,
