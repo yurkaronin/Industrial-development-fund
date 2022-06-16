1457a0e794b909d239a506e45a054d2c00a0ec59
RoninsWork <yurkaronin@gmail.com>
2022-06-16 12:18:19 +0300

    Правки позиционирования мобильного меню

--- js/parts/fixed-header.js
+++ js/parts/fixed-header.js
@@ -1,8 +1,8 @@
 window.onscroll = function headerFixed() {
     if(window.pageYOffset > 200) {
-      document.body.classList.add('js-desctop-menu-fixed');
+      document.body.classList.add('js-menu-fixed');
     } else {
-      document.body.classList.remove('js-desctop-menu-fixed');
+      document.body.classList.remove('js-menu-fixed');
     }
 
 }
